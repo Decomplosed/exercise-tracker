15cebce93cbf21e3ffa6107a3e10831f030936f3
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-02 11:22:46 +0200

    created a root route for exercises

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,2 +1,8 @@
 const router = require('express').Router()
 let Exercise = require('../models/exercise.model')
+
+router.route('/').get((req, res) => {
+  Exercise.find()
+    .then(exercises => req.json(exercises))
+    .catch(err => res.status(400).json('Errror: ' + err))
+})
