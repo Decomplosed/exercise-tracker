dc95726daacaa617fe210b32b7a0a3260655e082
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-02 11:24:33 +0200

    created a route of /add

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -6,3 +6,17 @@ router.route('/').get((req, res) => {
     .then(exercises => req.json(exercises))
     .catch(err => res.status(400).json('Errror: ' + err))
 })
+
+router.route('/add').post((req, res) => {
+  const username = req.body.username
+  const description = req.body.description
+  const duration = Number(req.body.duration)
+  const date = Date.parse(req.body.date)
+
+  const newExercise = new Exercise({
+    username,
+    description,
+    duration,
+    date
+  })
+})
