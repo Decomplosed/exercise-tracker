6ab8dcb827cc1279c59f9a2f90fc0f06be44ebfd
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-02 11:19:19 +0200

    created /add route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,3 +6,14 @@ router.route('/').get((req, res) => {
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err))
 })
+
+router.route('/add').post((req, res) => {
+  const username = req.body.username
+
+  const newUser = new User({ username })
+
+  newUser
+    .save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
