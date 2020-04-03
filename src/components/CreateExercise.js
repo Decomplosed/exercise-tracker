10cbe9056b6c8831826a9108b288ffe18197b90b
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-03 10:34:10 +0200

    created constructor and defined state

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 class CreateExercise extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
+    }
+  }
+
   render() {
     return <div>CreateExercise</div>
   }
