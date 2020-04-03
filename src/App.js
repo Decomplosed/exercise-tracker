20fc734f53bb1bb97a9d6679c5f593508fe9f738
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-03 09:39:59 +0200

    created Routes structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
-  return <div className='App'></div>
+  return
+  (<Router>
+    <Navbar />
+    <br />
+    <Route path='/' exact component={ExercisesList} />
+    <Route path='/edit/:id' component={EditExercise} />
+    <Route path='/create' component={CreateExercise} />
+    <Route path='/user' component={CreateUser} />
+  </Router>)
 }
 
 export default App
