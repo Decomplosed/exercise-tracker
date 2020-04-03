39de7f3c6498f9e7d178de3b0228262dad6a17ba
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-03 10:45:55 +0200

    added componentDidMount with test values

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -19,6 +19,13 @@ class CreateExercise extends React.Component {
     this.onSubmit = this.onSubmit.bind(this)
   }
 
+  componentDidMount() {
+    this.setState({
+      users: ['test user'],
+      username: 'test user'
+    })
+  }
+
   onChangeUsername(e) {
     this.setState({
       username: e.target.value
