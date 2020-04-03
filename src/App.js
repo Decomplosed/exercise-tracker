7f24c04846b72fcd8443529c610cfa18a8d61449
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-03 09:43:39 +0200

    fixed formatting

--- src/App.js
+++ src/App.js
@@ -9,15 +9,16 @@ import CreateExercise from './components/CreateExercise'
 import CreateUser from './components/CreateUser'
 
 function App() {
-  return
-  ;<Router>
-    <Navbar />
-    <br />
-    <Route path='/' exact component={ExercisesList} />
-    <Route path='/edit/:id' component={EditExercise} />
-    <Route path='/create' component={CreateExercise} />
-    <Route path='/user' component={CreateUser} />
-  </Router>
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path='/' exact component={ExercisesList} />
+      <Route path='/edit/:id' component={EditExercise} />
+      <Route path='/create' component={CreateExercise} />
+      <Route path='/user' component={CreateUser} />
+    </Router>
+  )
 }
 
 export default App
