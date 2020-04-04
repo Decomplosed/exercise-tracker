dce0b9815453ee465478279ab81508ae274e3fd9
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-04 14:58:49 +0200

    added template to the ExercisesList

--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -33,7 +33,23 @@ class ExercisesList extends React.Component {
   }
 
   render() {
-    return <div>ExercisesList</div>
+    return (
+      <div>
+        <h3>Logged Exercises</h3>
+        <table className='table'>
+          <thead className='thead-light'>
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          {/* <tbody>{this.exerciseList()}</tbody> */}
+        </table>
+      </div>
+    )
   }
 }
 
