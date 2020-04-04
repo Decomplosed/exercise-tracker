f8576cc68b3f09c776023269e32cbc3cad025683
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-04 14:36:50 +0200

    created small form for user creationg

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -27,11 +27,36 @@ class CreateUser extends React.Component {
 
     console.log(user)
 
-    window.location = '/'
+    this.setState({
+      username: '',
+    })
   }
 
   render() {
-    return <div>CreateUser</div>
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className='form-group'>
+            <label>Username: </label>
+            <input
+              type='text'
+              required
+              className='form-control'
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='submit'
+              value='Create User'
+              className='btn btn-primary'
+            />
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
