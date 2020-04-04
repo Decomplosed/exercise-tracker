8e5a0f9f5f8f162f1a0baa967ad99620cc3bb9b3
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-04 14:33:42 +0200

    copied constructor from create exercise and adjusted it to the user scenario

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,6 +1,45 @@
 import React from 'react'
 
 class CreateUser extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+    }
+
+    this.onChangeUsername = this.onChangeUsername.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date,
+    }
+
+    console.log(exercise)
+
+    window.location = '/'
+  }
+
+  componentDidMount() {
+    this.setState({
+      users: ['test user'],
+      username: 'test user',
+    })
+  }
+
   render() {
     return <div>CreateUser</div>
   }
